a33f562c7a6f2d732926a1f6dc96cf0f75210535
Diego <diegohxxiv@gmail.com>
2024-06-05 15:04:03 -0700

    api v1

--- controllers/posts.controller.ts
+++ controllers/posts.controller.ts
@@ -31,3 +31,38 @@ export const getPost = async (req: Request, res: Response) => {
     res.send({ error }).status(500);
   }
 };
+
+export const createPost = async (req: Request, res: Response) => {
+  const body = req.body;
+  try {
+    await prisma.post.create({
+      data: {
+        userId: body.userId,
+        userName: body.userName,
+        userAvatar: body.userAvatar,
+        content: body.content,
+      },
+    });
+    res.send({ message: "Post created" }).status(200);
+  } catch (error) {
+    res.send(error).status(500);
+  }
+};
+
+export const createComment = async (req: Request, res: Response) => {
+  const body = req.body;
+  try {
+    await prisma.comment.create({
+      data: {
+        postId: body.postId,
+        userId: body.userId,
+        userName: body.userName,
+        userAvatar: body.userAvatar,
+        content: body.content,
+      },
+    });
+    res.send({ message: "Comment created" });
+  } catch (error) {
+    res.send(error).status(500);
+  }
+};
--- routes/posts.route.ts
+++ routes/posts.route.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { getPosts, getPost } from "../controllers/posts.controller";
+import {
+  getPosts,
+  getPost,
+  createPost,
+  createComment,
+} from "../controllers/posts.controller";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
+router.post("/add-post", createPost);
+router.post("/add-comment", createComment);
 
 export default router;
